429a9316d9532a8c316668d2235e9020674048a5
Jgalvan <galvan.jgg@gmail.com>
2022-09-14 12:00:50 +0100

    Se mejora la lógica para comprobar la palabra introducida, se valora si existen varias letras iguales y si estas están en la posición correcta o no

--- js/wordle.js
+++ js/wordle.js
@@ -8,10 +8,31 @@ ContainerWord.className = "containerWordHidden";
 const Keyboard = document.createElement("article");
 Keyboard.id = "keyboard";
 Keyboard.className = "containerKeyboard";
+const ContainerMenu = document.createElement("span");
+ContainerMenu.id = "modal";
+ContainerMenu.className = "modalMenu";
 ContainerGame.append(ContainerWord,Keyboard);
 
 // Creamos la palabra oculta y los intentos para adivinarla
-const wordToQuest = ["j","e","s","u","s"];
+const WordBase = [
+    ["pato","rana","papa","mama","loro","vaca","azul","rosa","rojo","mono","mesa","leer"],
+    ["raton","prima","planta","arbol","lapiz","avion","cebra","mosca","negro","verde","letra","libro","reloj","coche","viaje","jugar","jesus"],
+    ["abuelo","abuela","thiago","blanco","letras","viajar","correr","futbol","juegos","pintar","cantar","bailar","reirse","musica"]
+];
+const newWord = () => {
+    let base1 = aleatorio(1,3);
+    let dimensionOne = base1 - 1;
+    let max = WordBase[dimensionOne].length;
+    let base2 = aleatorio(1,max);
+    let wordSelected = base2 - 1;
+    let word = WordBase[dimensionOne][wordSelected];
+    let secretWord = word.split("");
+    for(letter of secretWord) {
+        wordToQuest.push(letter.toUpperCase());
+    }
+}
+const wordToQuest = [];
+newWord();
 let attempts = 0;
 const rows = [];
 for(let row = 0; row < 5; row++) {
@@ -45,7 +66,7 @@ KeyboardLetters.map((letters) => {
         const ListItem = document.createElement("li");
         ListItem.className = "listLetters__item";
         ListItem.innerHTML = `
-            <button onclick="pressLetter()" id=${letter} class="buttonKeyboard" >${letter}</button>
+            <button onclick="pressLetter()" id=${letter.toUpperCase()} class="buttonKeyboard" >${letter.toUpperCase()}</button>
         `;
         List.appendChild(ListItem);
     });
@@ -58,10 +79,10 @@ Keyboard.append(...ListElements);
 const pressLetter = () => {
     const button = event.target;
     switch(button.id){
-        case "enter":
+        case "ENTER":
             checkWord();
             break;
-        case "delete":
+        case "DELETE":
             deleteLetter();
             break;
         default:
@@ -84,10 +105,31 @@ const checkWord = () => {
                 positions.push("green");
                 break;
             case wordToQuest.includes(myAnswer[i]):
-                positions.push("yellow");
+                for(position in wordToQuest) {
+                    positionLetter = wordToQuest[position];
+                    if(positionLetter === myAnswer[i]){
+                        let colorPosition = wordToQuest.indexOf(positionLetter,position);
+                        if(positions[colorPosition] != "green") {
+                            positions.push("yellow");
+                        } else {
+                            let countLetter = wordToQuest.reduce((count,item) => {
+                                if(!count[item]) {
+                                    count[item] = 1;
+                                } else {
+                                    count[item]++;
+                                }
+                                return count;
+                            },{});
+                            if(countLetter[myAnswer[i]] === 1){
+                                positions.push("grey");
+                            }
+                        }
+                    }
+                }
                 break;
             default:
                 positions.push("grey");
+                break;
         }
     }
     if(myAnswer.join("") === wordToQuest.join("")){
@@ -105,4 +147,9 @@ const deleteLetter = () => {
     myAnswer.pop();
     const deleteItem = document.getElementById(`${attempts}-${myAnswer.length}`)
     deleteItem.textContent = "";
+}
+
+function aleatorio(min, max) {
+    var resultado = Math.floor(Math.random() * (max - min + 1)) + min;
+    return resultado;
 }
